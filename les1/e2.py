2af0b823d084082532583970e9de212288d2f70a
zhen <zhen.chen@ogd.nl>
2018-08-14 16:54:56 +0200

    some chanegs

--- les1/e2.py
+++ les1/e2.py
@@ -8,7 +8,6 @@ print("""
 """)
 octets = ip_addr1.split(".")
 print("{:^15}{:^15}{:^15}{:^15}".format(*octets))
-
 print("{:^15}{:^15}{:^15}{:^15}".format(bin(int(octets[0])), bin(int(octets[1])),bin(int(octets[2])), bin(int(octets[3]))))
 print("{:^15}{:^15}{:^15}{:^15}".format(hex(int(octets[0])), hex(int(octets[1])),hex(int(octets[2])), hex(int(octets[3]))))
-print("------------------------------------------------------------")
+print("-" * 60)
--- les1/e3.py
+++ les1/e3.py
@@ -1,8 +1,8 @@
 from __future__ import print_function, unicode_literals
 
-ip_addr1 = u"fd00:8::ac1e:1f4b"
-IP_ADDR2 = u"fd00:8::ac1e:2f4b"
-Ip_Addr3 = u"fd00:8::ac1e:3f4b"
+ip_addr = "fd00:8::ac1e:1f4b"
+IP_ADDR = "fd00:8::ac1e:2f4b"
+Ip_Addr3 = "fd00:8::ac1e:3f4b"
 
-print(ip_addr1 == IP_ADDR2)
-print(ip_addr1 != Ip_Addr3)
+print(ip_addr == IP_ADDR)
+print(ip_addr != Ip_Addr3)
