46825a8bcd082548a464fecd9debefcf265a6e10
zhen <zhen.chen@ogd.nl>
2018-08-15 16:52:31 +0200

    implement IP address validation

--- les1/e2.py
+++ les1/e2.py
@@ -1,13 +1,22 @@
 from __future__ import print_function, unicode_literals
+import ipaddress
 
-ip_addr1 = raw_input("Please enter an IP address:")
+try:
+    # PY2
+    ip_addr1 = raw_input("Please enter an IP address: ")
+except NameError:
+    # PY3
+    ip_addr1 = input("Please enter an IP address: ")
+try:
+    ipaddress.ip_address(ip_addr1)
+    print("Valid IP")
+    print("""    Octet1         Octet2         Octet3         Octet4     
+    ------------------------------------------------------------""")
+    octets = ip_addr1.split(".")
+    print("{:^15}{:^15}{:^15}{:^15}".format(*octets))
+    print("{:^15}{:^15}{:^15}{:^15}".format(bin(int(octets[0])), bin(int(octets[1])),bin(int(octets[2])), bin(int(octets[3]))))
+    print("{:^15}{:^15}{:^15}{:^15}".format(hex(int(octets[0])), hex(int(octets[1])),hex(int(octets[2])), hex(int(octets[3]))))
+    print("-" * 60)
+except ValueError:
+    print("Invalid IP")
 
-print("""
-    Octet1         Octet2         Octet3         Octet4     
-------------------------------------------------------------
-""")
-octets = ip_addr1.split(".")
-print("{:^15}{:^15}{:^15}{:^15}".format(*octets))
-print("{:^15}{:^15}{:^15}{:^15}".format(bin(int(octets[0])), bin(int(octets[1])),bin(int(octets[2])), bin(int(octets[3]))))
-print("{:^15}{:^15}{:^15}{:^15}".format(hex(int(octets[0])), hex(int(octets[1])),hex(int(octets[2])), hex(int(octets[3]))))
-print("-" * 60)
